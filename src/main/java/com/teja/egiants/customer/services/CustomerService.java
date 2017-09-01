74aeea105080a41687289a0af2c3d7e84e236e0d
bunnymohan <mohansai.ijjada@gmail.com>
2017-09-01 03:00:50 -0500

    first commit

---
+++ src/main/java/com/teja/egiants/customer/services/CustomerService.java
@@ -0,0 +1,17 @@
+package com.teja.egiants.customer.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.teja.egiants.customer.dao.CustomerDAO;
+
+public class CustomerService 
+{
+	@Autowired
+	CustomerDAO customerDAO;
+
+	@Override
+	public String toString() {
+		return "CustomerService [customerDAO=" + customerDAO + "]";
+	}
+		
+}
